c0f61df93d9ddd9f055cec9f38254a440e3bb53d
Vincenzo- <viulenzgg@gmail.com>
2022-05-31 11:57:14 +0200

    Exercise2

--- src/films.js
+++ src/films.js
@@ -6,7 +6,16 @@ function getAllDirectors(array) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {}
+function getMoviesFromDirector(array, director) {
+  let result = array.filter((item) => {
+    if (item.director === director) {
+      return item.title;
+    }
+  });
+
+  console.log('EXERCICE 2 ->', result);
+  return result;
+}
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {}
